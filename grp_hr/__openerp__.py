1562d673261d7de5cccc1ee4aaa738cd631b93a6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-26 08:55:18 -0600

    [ADD] grp_hr

---
+++ grp_hr/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Enterprise Management Solution
+#    GRP Estado Uruguay
+#    Copyright (C) 2017 Quanam (ATEL SA., Uruguay)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name':     u'GRP - HR',
+    'author':   'Quanam',
+    'website':  'http://www.quanam.com',
+    'category': 'GRP',
+    'license':  'AGPL-3',
+    'version':  '1.0',
+    'description': u"""
+GRP - HR
+========
+
+""",
+    'depends' : ['hr',],
+    'data' : [
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+}
---
+++ grp_hr/models/hr.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Enterprise Management Solution
+#    GRP Estado Uruguay
+#    Copyright (C) 2017 Quanam (ATEL SA., Uruguay)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+
+class hrDepartment(models.Model):
+    _inherit = 'hr.employee'
+
+    is_patronato = fields.Boolean(string="Patronato")
+    is_comision_apoyo = fields.Boolean(string=u"Comisión de apoyo")
+    is_otros = fields.Boolean(string="Otros")
+    detalle_otros = fields.Char(string="Detalle de Otros", size=20)
+
