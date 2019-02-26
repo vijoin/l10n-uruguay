715537d7cf1a8e63b9fb4a8e78b1644116d62a21
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-26 08:55:18 -0600

    [ADD] uy_account_financial_report_aeroo_xls

---
+++ uy_account_financial_report_aeroo_xls/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Enterprise Management Solution
+#    GRP Estado Uruguay
+#    Copyright (C) 2017 Quanam (ATEL SA., Uruguay)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Inherit Account Financial Report Aeroo',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'author': 'Quanam',
+    'website':  'http://www.quanam.com',
+    'description': """
+
+    """,
+    'depends': ['account_financial_report_aeroo_xls'],
+    'data': [
+       'wizard/wizard_trail_balance_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
